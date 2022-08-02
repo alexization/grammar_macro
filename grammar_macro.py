694e1b9e0f2044ca11af8eeddf69e6d9c846d402
hyoseok <alexization@gmail.com>
2022-08-03 01:34:01 +0900

    grammar_macro.py

--- grammar_macro.py
+++ grammar_macro.py
@@ -14,26 +14,32 @@ class grammar():
         self.driver.implicitly_wait(5)
 
     def spell_check(self, str_before):
-        self.driver.find_element(By.ID, 'text1').send_keys(str_before)
-        self.driver.find_element(By.ID, 'btnCheck').click()
-        text_body = self.driver.find_element(By.XPATH, 
-        '/html/body/table/tbody/tr[2]/td').text
-
-        if text_body[0:3] == '맞춤법':
-            self.driver.find_element(By.ID, 'btnRenew2').click()
-            return str_before
-
-        count = 1
         while True:
             try:
-                self.driver.find_element(By.XPATH, 
-                f"/html/body/table[1]/tbody/tr[2]/td/table[1]/tbody/tr[2]/td[1]/div/table/tbody/tr/td/font[{count}]/span").click()
-                count += 1
+                self.driver.find_element(By.ID, 'text1').send_keys(str_before)
+                self.driver.find_element(By.ID, 'btnCheck').click()
+                text_body = self.driver.find_element(By.XPATH, 
+                '/html/body/table/tbody/tr[2]/td').text
 
+                if text_body[0:3] == '맞춤법':
+                    self.driver.find_element(By.ID, 'btnRenew2').click()
+                    return str_before
+
+                count = 1
+                while True:
+                    try:
+                        self.driver.find_element(By.XPATH, 
+                        f"/html/body/table[1]/tbody/tr[2]/td/table[1]/tbody/tr[2]/td[1]/div/table/tbody/tr/td/font[{count}]/span").click()
+                        count += 1
+
+                    except:
+                        break
+                
+                str_after = self.driver.find_element(By.XPATH, "/html/body/table[1]/tbody/tr[2]/td/table[1]/tbody/tr[2]/td[1]/div/table/tbody/tr/td").text
+                self.driver.find_element(By.ID, 'btnRenew2').click()
+                
+                return str_after
+            
             except:
-                break
-        
-        str_after = self.driver.find_element(By.XPATH, "/html/body/table[1]/tbody/tr[2]/td/table[1]/tbody/tr[2]/td[1]/div/table/tbody/tr/td").text
-        self.driver.find_element(By.ID, 'btnRenew2').click()
-        
-        return str_after
+                self.driver.refresh()
+                continue
